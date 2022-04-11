aeaf11ce8129d63002e31544f3edd4edfb779675
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-11 15:43:08 -0500

    get and post endpoints implemented

--- api/server.js
+++ api/server.js
@@ -1,6 +1,58 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
 
+const User = require('./users/model')
+
 const server = express();
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+    let user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    } else {
+        User.insert(user)
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(() => {
+                res.status(500).json({ message: "There was an error while saving the user to the database" })
+            })
+    }
+});
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The users information could not be retrieved" })
+        })
+});
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            } else {
+                res.json(user)
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The user information could not be retrieved" })
+        })
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    res.json("not implemented");
+});
+
+server.put('/api/users/:id', (req, res) => {
+    res.json("not implemented");
+});
+
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
