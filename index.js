9cd6b1927626d80c4e9c75467edcc2f2fe4484f7
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-11 15:07:36 -0500

    added server script that runs nonfunctional API using nodemon

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
-//
+server.listen(port, () => {
+    console.log(`server is now listening of port ${port}`)
+});
